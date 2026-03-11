1c0a3556ee4c2dc96d4d550bf7dcef304376e181
maxiday123-dotcom <maxiday123@gmail.com>
2026-03-11 00:15:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,34 @@ duration:1.1,
 ease:"power2.out"
 })
 })
+
+const cards = document.querySelectorAll(".card")
+
+cards.forEach(card => {
+
+card.addEventListener("mousemove", e => {
+
+const rect = card.getBoundingClientRect()
+
+const x = e.clientX - rect.left
+const y = e.clientY - rect.top
+
+const centerX = rect.width / 2
+const centerY = rect.height / 2
+
+const rotateX = -(y - centerY) / 15
+const rotateY = (x - centerX) / 15
+
+card.style.transform =
+`perspective(900px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`
+
+})
+
+card.addEventListener("mouseleave", () => {
+
+card.style.transform =
+"perspective(900px) rotateX(0) rotateY(0) scale(1)"
+
+})
+
+})
