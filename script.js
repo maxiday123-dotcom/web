85a0f8bf1e8d4c21a7089bbe5e03fba7f39f8ab4
maxiday123-dotcom <maxiday123@gmail.com>
2026-03-11 01:08:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -101,3 +101,22 @@ card.style.transform =
 })
 
 })
+
+/* SCROLL SUAVE */
+
+const lenis = new Lenis({
+
+duration:1.2,
+smooth:true,
+
+})
+
+function raf(time) {
+
+lenis.raf(time)
+
+requestAnimationFrame(raf)
+
+}
+
+requestAnimationFrame(raf)
